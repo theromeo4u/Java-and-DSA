a1a5b22041418afcf0ba66cfb705da7ac4c245b9
Subodh Kumar <snp42u@gmail.com>
2022-08-21 13:41:10 +0530

    Commit

--- sumOfDigit.java
+++ sumOfDigit.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
 
 public class sumOfDigit {
     int sum=0;
 	public int sumOfDigits(int number){
-	    for (sum = 0; number > 0; sum = sum + number % 10, number = number / 10);
+	    for (sum = 0; number > 0;)
+		{
+			sum = sum + number % 10;
+			number = number / 10;
+		}
+
 	    return sum;
 	}
-}
-
-class Tester {
-
 	public static void main(String args[]) {
-	    int num=7812089;
+	    int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number to be addition: ");
+		num = sc.nextInt();
 		sumOfDigit val = new sumOfDigit();
 		System.out.println(val.sumOfDigits(num));
-
+		sc.close();
 	}
 }
